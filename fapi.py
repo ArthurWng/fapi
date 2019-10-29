ac854bfa9889805b0093864986e92ac90c570560
arthur wang <2629917751@qq.com>
2019-10-29 10:45:20 +0800

    fixed Flake8 issues

--- fapi.py
+++ fapi.py
@@ -3,7 +3,7 @@
 from flask import Flask, request  # 导入Flask类库和request类库
 
 app = Flask('fapi')  # 实例化，定义一个Flask实例
-@app.route('/get_request_info', methods=['post','get'])
+@app.route('/get_request_info', methods=['post', 'get'])
 def get_request_info():
     request_info = dict()
     request_info['headers'] = dict(request.headers)
@@ -12,6 +12,7 @@ def get_request_info():
     request_info['params_query'] = dict(request.args)
 
     return request_info
+
 
 if __name__ == '__main__':
     app.run(port=4500, debug=True, host='0.0.0.0')  # host='0.0.0.0' 表示内网和外网都可以访问本Flask服务
